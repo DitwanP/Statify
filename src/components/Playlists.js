87ebf19f0e9d82b5c44bc473019c65cdc1cc39b6
DitwanP <price.ditwan@gmail.com>
2021-01-04 20:14:45 -0500

    Done with the playlist page.

--- src/components/Playlists.js
+++ src/components/Playlists.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { motion } from "framer-motion";
 import '../styles/Playlists.scss';
+import { getPlaylists } from '../spotify';
+import { catchErrors } from '../utils';
+import Loader from './Loader';
 
 const pageVariants = { 
     in:{ 
@@ -25,21 +28,56 @@ const pageTransitions = {
 
 
 export default class Playlists extends Component {
-    render() {
-        return (
-            <motion.div className="playlists"
-            variants={pageVariants}
-            initial="notIn"
-            animate="in"
-            exit="outDown"
-            transition={pageTransitions}>
-                <div className="header-container">
-                    <h1>Your Playlists</h1>
-                </div>
-                <div className="users-playlists-container">
 
-                </div>
-            </motion.div>
+    state = {
+        playlists: null,
+    };
+
+    componentDidMount() {
+        catchErrors(this.getData());
+    }
+
+    async getData() {
+        const { data } = await getPlaylists();
+        this.setState({ playlists: data });
+    }  
+
+    render() {
+        const { playlists } = this.state;
+        console.log(playlists);
+
+        return (
+            <React.Fragment>
+                { playlists ? (
+                    <div>
+                        <motion.div className="playlists"
+                        variants={pageVariants}
+                        initial="notIn"
+                        animate="in"
+                        exit="outDown"
+                        transition={pageTransitions}>
+                            <div className="header-container">
+                                <h1>Your Playlists</h1>
+                            </div>
+                            <div className="users-playlists-container">
+                                {playlists.items.map((playlist, index) => 
+                                    <div key={index} className="playlist-item">
+                                        <a href={playlist.external_urls.spotify} className="playlist-image" target="_blank" rel="noopener noreferrer">
+                                            <img src={playlist.images[0].url} alt="playlist thumbnail"/>
+                                        </a>
+                                        <a href={playlist.external_urls.spotify} className="playlist-name" target="_blank" rel="noopener noreferrer">
+                                            <h1> {playlist.name} </h1>
+                                        </a>
+                                        <h2> {playlist.tracks.total} SONGS </h2>
+                                    </div>
+                                )}
+                            </div>
+                        </motion.div> 
+                    </div>
+                ) : (
+                    <Loader />
+                )}
+            </React.Fragment>
         )
     }
 }
